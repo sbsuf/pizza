b0d0606a459a9e0b66897d8f95c583e026a9e9e1
Justin Xu <justinjxu@yahoo.com>
2016-11-04 11:26:24 -0700

    add function prevent user submit empty form

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,30 @@ function Pizza(topping, size) {
  this.psize = size;
 }
 
-
 Pizza.prototype.calculate = function() {
   return this.ptopping + this.psize;
 }
 
 function resetFields() {
-    $("#new-first-name").val("");
-    $("#new-last-name").val("");
+    $("#topping").val("");
+    $("#size").val("");
 }
 
 $(document).ready(function() {
   $("form").submit(function(event) {
+      if ($("#topping").val()==="0" || $("#size").val()==="0") {
+        alert("Please select your pizza!");
+      }
+      else {
+      jQuery(this).unbind('submit'); // unbind this submit handler first and ...
+      var top = parseInt($("#topping").val());
+      var size = parseInt($("#size").val());
+      var newPizza = new Pizza(top, size);
+
+        $(".show").append("<p>Your pizza price is $" + newPizza.calculate() + "</p>" + "<br>"
+        + "<p>" + "Thanks for shopping with us" + "</p>");
+
+}
     event.preventDefault();
-
-    var top = parseInt($("#topping").val());
-    var size = parseInt($("#size").val());
-    var newPizza = new Pizza(top, size);
-
-    $(".show").append("<p>Your pizza price is $" + newPizza.calculate() + "</p>" + "<br>"
-    + "<p>" + "Thanks for shopping with us" + "</p>");
-
-    resetFields();
-
   });
 });
